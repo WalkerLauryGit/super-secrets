59140bdee6ec341c5d99f156b90153ed593ea616
walker laury <walkerclaury@protonmail.com>
2022-10-18 21:22:25 -0500

    Added image upload locally to front end in form.jsx, currently allows upload and showing of image

---
+++ src/components/Form.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export default function Form() {
+  const [message, setMessage] = useState('');
+  const [image, setImage] = useState();
+  const handleImageChange = (e) => {
+    setImage(URL.createObjectURL(e.target.files[0]));
+    console.log(image);
+  };
+
+  return (
+    <div>
+      <form>
+        <h2>Add your message:</h2>
+        <textarea
+          name='message'
+          id=''
+          cols='30'
+          rows='10'
+          onChange={() => setMessage(e.target.value)}
+        ></textarea>
+        <h2>Upload image</h2>
+        <input type='file' name='img' id='' onChange={handleImageChange} />
+        <button type='submit'>Submit</button>
+      </form>
+      <div>{image && <img src={image} alt='' />}</div>
+    </div>
+  );
+}
